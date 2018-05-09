7ac8afa8f0b8def9a8d52616d5068ea94ad4e967
damian <damian.popielarski@onwelo.com>
2018-05-09 03:20:47 +0200

    Added tests for required scenarios

--- src/test/groovy/VendingMachineSpec.groovy
+++ src/test/groovy/VendingMachineSpec.groovy
@@ -1,62 +1,186 @@
+import hardware.CoinHolder
+import domain.CoinType
+import domain.Product
+import domain.Transaction
+import domain.TransactionStatus
+import hardware.MachineDisplay
+import hardware.MachineProductPublisher
+import spock.lang.Shared
 import spock.lang.Specification
 
 class VendingMachineSpec extends Specification {
 
-    def setup() {
-        "Running tests..."
+    private VendingMachine vendingMachine
+
+    private MachineDisplay display
+
+    private MachineProductPublisher productPublisher
+
+    private CoinHolder coinHolder
+
+    @Shared
+    private Integer randomShelveNumber = 1
+
+    @Shared
+    private List<Product> randomProductsToAdd = Arrays.asList(
+            new Product("Fanta 0.5l", 3.50),
+            new Product("Chocolate", 5.50)
+    )
+
+    @Shared
+    private Integer firstIndex = 0
+
+    @Shared
+    private Integer secondIndex = 1
+
+    void setup() {
+        display = Spy(MachineDisplay.class)
+        productPublisher = Spy(MachineProductPublisher.class)
+        coinHolder = Spy(CoinHolder.class)
+        vendingMachine = Spy(VendingMachine.class,
+                constructorArgs: [display, productPublisher, coinHolder])
+    }
+
+    void cleanup() {
+        coinHolder.clear()
+        productPublisher.clear()
     }
 
     def "should not throw a product when shelve is empty"() {
-        when: "number of product is chosen"
+        setup:
+        Product fanta = randomProductsToAdd.get(firstIndex)
+        productPublisher.addProduct(randomShelveNumber, fanta)
+
+        when: "number of product's shelve is selected"
+        vendingMachine.selectShelve(randomShelveNumber)
 
         then: "price of product is displayed on a screen"
-
-        when: "put coins into vending machine"
-
-        then: "machine display warning about empty shelve"
+        1 * display.displayMessageAboutEmptyShelve(randomShelveNumber)
     }
 
-    def "should not return product when chosen coin is not one of the accepted denominations"() {
-        when: "number of product is chosen"
+    def "should show price to cover a product after insert a coin"() {
+        setup:
+        Product chocolate = randomProductsToAdd.get(secondIndex)
+        productPublisher.addProduct(randomShelveNumber, 10, chocolate)
 
-        then: "price of product is displayed on a screen"
+        BigDecimal expectedAmountToCoverAfterFirstInsert = new BigDecimal("4.50")
+        BigDecimal expectedAmountToCoverAfterSecondInsert = new BigDecimal("4.30")
 
-        when: "put coins into vending machine"
+        when: "number of product's shelve is selected"
+        vendingMachine.selectShelve(randomShelveNumber)
 
-        then: "machine display warning and returns coin"
+        and: "put coin into machine"
+        vendingMachine.putCoin(CoinType.ONE)
+        vendingMachine.putCoin(CoinType.ZERO_TWO)
 
+        then:
+        1 * display.displayMessageAboutPriceToCover(expectedAmountToCoverAfterFirstInsert)
+        1 * display.displayMessageAboutPriceToCover(expectedAmountToCoverAfterSecondInsert)
     }
 
     def "should get money back when user inserted insufficient money"() {
-        when: "number of product is chosen"
+        setup:
+        Product chocolate = randomProductsToAdd.get(secondIndex)
+        productPublisher.addProduct(randomShelveNumber, 10, chocolate)
+        BigDecimal expectedAmountToCoverPrice = new BigDecimal("4.20")
+
+        when: "number of product's shelve is selected"
+        vendingMachine.selectShelve(randomShelveNumber)
 
         then: "price of product is displayed on a screen"
+        1 * display.displayMessageAboutSelectedProduct(chocolate)
 
         when: "put coins into vending machine"
+        vendingMachine.putCoin(CoinType.ZERO_ONE)
+        vendingMachine.putCoin(CoinType.ZERO_TWO)
+        vendingMachine.putCoin(CoinType.ZERO_FIVE)
+        vendingMachine.putCoin(CoinType.ZERO_FIVE)
 
-        and: "cancel button is pressed"
+        then:
+        1 * display.displayMessageAboutPriceToCover(expectedAmountToCoverPrice)
 
-        then: "machine returns inserted coins"
+        and: "cancel button is pressed and machine return inserted coins"
+        vendingMachine.pressCancelButton()
+
+        Transaction transaction = getLastTransaction()
+
+        transaction.getStatus() == TransactionStatus.CANCELED
+        transaction.getRefundedCoins().containsAll(CoinType.ZERO_ONE,
+                CoinType.ZERO_TWO,
+                CoinType.ZERO_FIVE,
+                CoinType.ZERO_FIVE)
     }
 
     def "should get money back when vending machine does not have enough money to change"() {
-        when: "number of product is chosen"
+        setup:
+        Product chocolate = randomProductsToAdd.get(secondIndex)
+        productPublisher.addProduct(randomShelveNumber, 10, chocolate)
+
+        coinHolder.insertCoinsToChange(CoinType.ZERO_ONE, CoinType.ZERO_TWO)
+
+        when: "number of product's shelve is selected"
+        vendingMachine.selectShelve(randomShelveNumber)
 
         and: "put coins into vending machine"
+        vendingMachine.putCoin(CoinType.FIVE)
+        vendingMachine.putCoin(CoinType.ONE)
 
-        then: "machine returns coins because it does not contain coins to change"
+        then: "machine returns coins"
+        1 * vendingMachine.refund(_)
+
+        Transaction transaction = getLastTransaction()
+        transaction.getStatus() == TransactionStatus.INSUFFICIENT_MONEY
+        transaction.getRefundedCoins().containsAll(CoinType.FIVE, CoinType.ONE)
 
     }
 
-    def "should return change after purchase of cola "() {
-        when: "number of product is chosen"
+    def "should return change after purchase of product"() {
+        setup:
+        Product fanta = randomProductsToAdd.get(firstIndex)
+        productPublisher.addProduct(randomShelveNumber, 10, fanta)
 
-        and: "put coins into vending machine"
+        BigDecimal expectedAmountToCoverPrice = new BigDecimal("1.50")
+
+        List<CoinType> expectedChange = Arrays.asList(CoinType.ZERO_TWO,
+                CoinType.ZERO_TWO,
+                CoinType.ZERO_ONE)
+
+        coinHolder.insertCoinsToChange(CoinType.ONE,
+                CoinType.ZERO_TWO,
+                CoinType.ZERO_TWO,
+                CoinType.ZERO_ONE)
+
+        when: "number of product's shelve is selected"
+        vendingMachine.selectShelve(randomShelveNumber)
+
+        then: "price of product is displayed on a screen"
+        1 * display.displayMessageAboutSelectedProduct(fanta)
+
+        when: "put coin into vending machine"
+        vendingMachine.putCoin(CoinType.TWO)
+
+        then:
+        1 * display.displayMessageAboutPriceToCover(expectedAmountToCoverPrice)
+
+        when: "put another coin into vending machine"
+        vendingMachine.putCoin(CoinType.TWO)
 
         then: "machine returns product"
+        1 * vendingMachine.releaseProduct(_)
+        1 * productPublisher.releaseProduct(randomShelveNumber)
 
         and: "machine returns change"
+        1 * vendingMachine.returnChange(_)
 
+        Transaction transaction = getLastTransaction()
+
+        transaction.getStatus() == TransactionStatus.SUCCESS
+        expectedChange.containsAll(transaction.getCoinsToReturnChange())
+
+    }
+
+    private Transaction getLastTransaction() {
+        return vendingMachine.getTransactions().get(firstIndex)
     }
 
 
